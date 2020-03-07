e9ceaf716eb4fa02111f063040737a673eb6ec9c
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-03-07 16:41:50 -0500

    formatting

--- src/main/java/company/employeeRestApi/controllers/EmployeeController.java
+++ src/main/java/company/employeeRestApi/controllers/EmployeeController.java
@@ -14,24 +14,24 @@ import company.employeeRestApi.entities.Employee;
 
 @RestController
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
-	@RequestMapping("/employees")
-	public List<Employee> getAllEmployees(){
+
+	@RequestMapping("/")
+	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
-		
+
 	}
-	
+
 	@RequestMapping("/employee/{employeeId}")
 	public Employee getEmployeeById(@PathVariable("employeeId") Integer employeeId) {
 		return employeeService.getEmployee(employeeId);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/employee")
-    public void addEmployee(@RequestBody Employee employee) {
-        employeeService.addEmployee(employee);
-    }
+
+	@RequestMapping(method = RequestMethod.POST, value = "/employee/addEmployee")
+	public void addEmployee(@RequestBody Employee employee) {
+		employeeService.addEmployee(employee);
+	}
 
 }
