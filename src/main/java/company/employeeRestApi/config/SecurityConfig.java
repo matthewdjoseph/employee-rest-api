382c71e4b0001b0f1dbfea4b51331b5225a46316
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2020-03-09 11:08:51 -0400

    security all set for deactivateEmployee call

--- src/main/java/company/employeeRestApi/config/SecurityConfig.java
+++ src/main/java/company/employeeRestApi/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.csrf().disable().authorizeRequests().antMatchers("/employee/deactivateEmployee/").authenticated().and().httpBasic();
 	}
 
 }
